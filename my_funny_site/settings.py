7f28a73538ecb6aab6173e33e5b493818fea6871
Andras <andras.vincze@yahoo.com>
2021-09-15 16:46:02 +0200

    update

--- my_funny_site/settings.py
+++ my_funny_site/settings.py
@@ -20,12 +20,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't4k5e_(twtudgofb%!9zu=mhniy7)abn3+i$+^zbb(#(_*uw*b'
+# SECRET_KEY = ''
+
+
+with open('/etc/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
 
 
 # Application definition
